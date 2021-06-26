b363fa3855df01257e8322f7f052cd0fd9081ee5
Tom Booker <mrtombooker@gmail.com>
2021-06-25 17:05:01 -0700

    Moved files around and updated the annotation of  WZA files

--- dataAnalysis/bin/WZA_AdapTree.py
+++ dataAnalysis/bin/WZA_AdapTree.py
@@ -4,35 +4,32 @@ import numpy as np
 import sys, glob, argparse
 from GEA_functions import *
 
-## Here's a script to calculate the Weighted Z score (and the top-candidate test from the CoAdapTree data structures)
+## Here's a script to calculate the Weighted Z score (and the top-candidate test from the AdapTree data structures - i.e. those that are present in the Dryad repository)
 
 class corLine:
 
-	def __init__(self, c):
+	def __init__(self, c, envIndex):
 
 		dat = c.strip().split()
 
-		self.contig = dat[0]
+		self.contig = dat[6]
 
-		self.pos = int(dat[1])
+		self.pos = int(dat[7])
 
 ## The data in the Correlations file are stored as contig-pos, so I'll parse that out
 ## Note that this is VERY sensitive to the file structure staying the same
-		self.env = dat[2]
+		if dat[envIndex] == "NA":
+				self.pVal = "NA"
+		else:
+				self.pVal = float(dat[envIndex])
 
-		self.rho = float(dat[3])
-
-		self.pVal = float(dat[4])
-
-		MAF = float(dat[5])
+		MAF =  1 - float(dat[13])
 
 		self.pbar_qbar = MAF * (1 - MAF) 
 
-		self.emp_pVal = float(dat[6])
-
-
 ## A generator function to spit out the consecutive lines for the contig
 def contigGenerator(correlationFile, envFilter):
+
 
 	current_contig = ""
 
@@ -41,11 +38,9 @@ def contigGenerator(correlationFile, envFilter):
 	with open( correlationFile ) as cor:
 		for c in cor:
 ## Ignore the header
-			if c.startswith("snp") or c.startswith("contig"):continue
+			if c.startswith("X.annotation"):continue
 			
-			currentLine = corLine(c)
-
-			if currentLine.env != envFilter: continue
+			currentLine = corLine(c, envFilter)
 
 			if currentLine.contig != current_contig:
 
@@ -69,34 +64,30 @@ def contigGenerator(correlationFile, envFilter):
 
 	yield current_contig, contig_data
 
-def contigSnpTable(snps, contig_dat):
+def contigSnpTable(snps):
 
 	data_for_table = []
 
 	for s in snps:
-
-		gene_bool = (s.pos>=contig_dat.start)&(s.pos<=contig_dat.end)
-
-		if sum(gene_bool) == 0:continue
-
-		gene_name = ":".join(list(contig_dat.attribute[gene_bool]))
-
-		gene_start = list(contig_dat.start[gene_bool] )[0]
-
-		gene_end = list( contig_dat.end[gene_bool] )[0]
-
-		if sum(gene_bool) > 1:
-			print("more than one gene overlapping with SNP:", s.contig, s.pos)
+		
+# 		gene_bool = (s.pos>=contig_dat.start)&(s.pos<=contig_dat.end)
+# 
+# 		if sum(gene_bool) == 0:continue
+# 
+# 		gene_name =s":".join(list(contig_dat.attribute[gene_bool]))
+# 		gene_start = list(contig_dat.start[gene_bool] )[0]
+# 
+# 		gene_end = list( contig_dat.end[gene_bool] )[0]
+# 
+# 		if sum(gene_bool) > 1:
+# 			print("more than one gene overlapping with SNP:", s.contig, s.pos)
 
 		data_for_table.append({"contig":s.contig,
 									"pos":s.pos,
-									"rho":s.rho,
+#									"rho":s.rho,
 									"pVal":s.pVal,
-									"empirical_pVal":s.emp_pVal,
 									"pbar_qbar":s.pbar_qbar,
-									"gene":gene_name,
-									"gene_start":gene_start,
-									"gene_end":gene_end})
+									"gene":s.contig})
 
 	return pd.DataFrame(data_for_table)	
 	
@@ -106,11 +97,10 @@ def correlationThreshold( corData, targetEnv, percentile_threshold = 99.9):
 	pValues = []
 	with open( corData ) as cor:
 		for c in cor:
-			if c.startswith("snp"):continue
-			currentLine = corLine(c)
-			if currentLine.env != targetEnv: continue
-			else:
-				pValues.append(currentLine.pVal)
+			if c.startswith("X.annotation") or c.startswith("X_annotation"):continue
+			currentLine = corLine(c, targetEnv)
+			if currentLine.pVal == "NA": continue
+			pValues.append(currentLine.pVal)
 	if len(pValues) == 0:
 		return None
 	else:
@@ -133,14 +123,6 @@ def main():
 
 			help = "The file containing the correlations")
 
-	parser.add_argument("--annotations","-a", 
-
-			required = True,
-
-			dest = "annotations", 
-
-			help = "The file of annotations. The script asssumes GFF as default, set the '--bed' flag if using that format")
-
 	parser.add_argument("--output", 
 
 			required = True,
@@ -159,80 +141,71 @@ def main():
 
 			type = str, 
 
-			help = "[OPTIONAL] If you want to analyse just a single environment, give it here")
-
-	parser.add_argument("--bed", 
+			help = "If you want to analyse just a single environment, give it here [DD_0]",
+			
+			default = "DD_0")
+			
+<<<<<<< HEAD
+	parser.add_argument("--empirical_p", 
 
 			required = False,
 
-			dest = "bed",
+			dest = "empirical_p",
 
 			action = "store_true", 
 
-			help = "[OPTIONAL] Give this flag if the analysis files are in BED format. Otherwise the script assumes GFF format")
+			help = "[OPTIONAL] Give this flag if you want to analyse empirical p-values that have been appended to the dataframe")
+=======
+	parser.add_argument("--bay", 
+
+			required = False,
+
+			dest = "bay",
+
+			action = "store_true", 
+
+			help = "[OPTIONAL] Give this flag if the analysis files are BayesFactors from bayEnv.")
+>>>>>>> origin/master
 
 	args = parser.parse_args()
-	
-	if args.bed:
-		annotations = pd.read_csv(args.annotations , 
-
-					sep = "\t",
-
-						names = ["seqname",
-
-								"start",
-
-								"end",
-
-								"attribute"])
-## Add 1 to the positions to make correct for 0-based BedTools
-		annotations["start"] +=1
-
-	else:
-## GFF header from ENSEMBL webpage
-		annotations = pd.read_csv(args.annotations , 
-
-					sep = "\t",
-
-						names = ["seqname",
-
-								"source",
-
-								"feature",
-
-								"start",
-
-								"end",
-
-								"score",
-
-								"strand",
-
-								"frame",
-
-								"attribute"])
 
 ## For all environmental variables:
 ## MAT MWMT MCMT TD MAP MSP AHM SHM DD_0 DDS NFFD bFFP FFP PAS EMT EXT Eref CMD
 
-	envs =["MAT","MWMT","MCMT","TD",
-			"MAP","MSP","AHM","SHM",
-			"DD_0","DD5","NFFD","bFFP",
-			"FFP","PAS","EMT","EXT","Eref","CMD"]
+	adapTreeEnvs = ["LAT"	,"LONG"	,"ELEVATION"	,"MAT"	,"MWMT"	,"MCMT"	,"TD"	,"MAP"	,"MSP"	,"AHM"	,"SHM"	,"DD_0"	,"DD5"	,"NFFD"	,"bFFP"	,"eFFP"	,"FFP"	,"PAS"	,"EMT"	,"EXT"	,"Eref"	,"CMD"	,"Budset_p"	,"Budbreak_p"	,"Height_season_1_p"	,"Height_season_2_p"	,"Diameter_p"	,"Shoot_weight_p"	,"Root_weight_p"	,"Max_growth_rate_p"	,"Linear_growth_days_p"	,"X5_growth_complete_days_p"	,"X95_growth_complete_days_p"	,"X5_95_growth_days_p"	,"Fall_cold_injury_p"	,"Winter_cold_injury_p"	,"Spring_cold_injury_p"	,"root_wt_shoot_wt_p"	,"root_wt_shoot_wt_p_1"]
+
+	adapTreeEnvDict = {}
+	
+	for i in range(len(adapTreeEnvs)):
+		adapTreeEnvDict[adapTreeEnvs[i]] = i + 32 ## to adjust for the length of the adaptree SNP table files
+
 ## If you specify just a single environment (which is recommended for parallelisation)
 	if args.env:
 ## Make sure that the environment specified is actually valid
-		if args.env not in envs:
+		if args.env not in adapTreeEnvs:
 			print("you did not specify a valid environment. Take a look at your command, bozo")
 			return
 		envs = [args.env]
 
 ## We're going to analyse each env. separately
 	for env in envs:
+<<<<<<< HEAD
+## If you've made the file of empirical ps, only analyse the last column
+		if args.empirical_p:
+=======
+		if args.bay:
+>>>>>>> origin/master
+			envIndex = -1
+		else:	
+			envIndex = adapTreeEnvDict[env]
 		all_contigs = []
 
 ## Now let's calculate the outlier threshold (for the TC test) from the data - Make sure it's a percentile!
-		threshold_99th = correlationThreshold( args.correlations, env, percentile_threshold = 99)
+		threshold_99th = correlationThreshold( args.correlations, envIndex, percentile_threshold = 99)
+<<<<<<< HEAD
+
+=======
+>>>>>>> origin/master
 		if threshold_99th == None:
 			print("Something went wrong when identifying the outlier threshold")
 			return
@@ -241,63 +214,83 @@ def main():
 
 		print("Analysing:",env)
 		
-## Iterate over contigs spat out by the 
-		for contig,SNPs in contigGenerator(args.correlations, env):
+		count = 0
+		
+<<<<<<< HEAD
+## Iterate over contigs spat out by the contig generator
+		for contig,SNPs in contigGenerator(args.correlations, envIndex):
+			count += 1
+#			if count ==100: break
+
 ## Grab all the genes present on this contig
-			contigDF = contigSnpTable(SNPs, annotations[annotations["seqname"] == contig])
-		#	print(contigDF)
+			contigDF = contigSnpTable(SNPs)
+			print(contig, count)
+=======
+## Iterate over contigs spat out by the 
+		for contig,SNPs in contigGenerator(args.correlations, envIndex):
+			count += 1
+			print(contig, count)
+			if count ==100: break
+
+## Grab all the genes present on this contig
+			contigDF = contigSnpTable(SNPs)
+>>>>>>> origin/master
+
+# Remove all NAs from the SNP set
+			contigDF  = contigDF[contigDF["pVal"]!="NA"]
 ## If there are no annotations on the current contig, move to the next
 			if contigDF.shape == (0,0):
 				continue
-
+			if contigDF.shape[0] <5: continue
 ## Get the average position of each annotation - not used for the analysis, just for downstream plotting
 
 			position = contigDF.groupby(["gene"])["pos"].mean().to_frame()
 
-			anno_start = contigDF.groupby(["gene"])["gene_start"].mean().to_frame()
 			
-			anno_end = contigDF.groupby(["gene"])["gene_end"].mean().to_frame()
-
-
-## Perform the WZA on the annotations in the contig using parametric ps
-			wza_pVal = WZA(contigDF, "pVal", varName = "Z_p")
-
-## Perform the WZA on the annotations in the contig using empirical ps
-			wza_emp_pVal = WZA(contigDF, "empirical_pVal", varName = "Z_empP")
-
+## Perform the WZA on the annotations in the contig
+			wza = WZA(contigDF, "pVal")
 ## Perform the top-candidate for the annotations in the contig
 			TopCan = top_candidate( contigDF, threshold_99th, 0.01, "pVal", 0.9999, MAF_filter = 0.05 )
-
 ## Combine the results
-			result = pd.concat([ position, wza_pVal, wza_emp_pVal, TopCan, anno_start, anno_end] , axis = 1, sort = True ).reset_index()
+			result = pd.concat([ position, wza, TopCan] , axis = 1, sort = True ).reset_index()
 
 			result["contig"] = contig
 
 			result["env"] = env
 
 			all_contigs.append( result )
+
+			if count%1000 == 0:
+				print( count,"contigs analysed")
+			
 			
 		if len( all_contigs ) == 0: continue 
 		
 ## Combine all contig-specific dataframes into a single big one
 		outputDF = pd.concat(all_contigs)
-		
+
 ## Get the expected proportion of hits per hit-bearing gene
 		expected = outputDF[outputDF["hits"]!=0]["hits"].sum() / outputDF[outputDF["hits"]!=0]["SNPs"].sum()
 
 		print("expected proportion", expected)
-
 		top_candidate_p = [ scipy.stats.binom_test(h, s, expected, alternative = "greater" ) for h, s in zip(outputDF.hits, outputDF.SNPs)]
 		
 		outputDF["top_candidate_p"] = top_candidate_p 
-
-#		expected_hits = [ scipy.stats.binom.ppf( 0.9999 , s, expected )  for s in outputDF.SNPs]
+#		outputDF["top_candidate_p"] = scipy.stats.binom_test(outputDF.hits, outputDF.SNPs, expected, alternative = "greater" ) 
+		expected_hits = [ scipy.stats.binom.ppf( 0.9999 , s, expected )  for s in outputDF.SNPs]
 
 		outputDF["expected_hits"] =  scipy.stats.binom.ppf( 0.9999 , outputDF.SNPs, expected )  
 
-		
+## Calculate the top-candidate index
+#		outputDF = pd.concat(all_contigs)		
+
+
 ## Write the dataframe to an output file
 		outputDF.to_csv(env + "_" + args.output,index = False)
 
 
 main()
+
+
+
+
---
+++ dataAnalysis/bin/annotateWZAfiles.py
@@ -0,0 +1,123 @@
+import argparse
+import pandas as pd
+
+def main():
+	parser = argparse.ArgumentParser(description="")
+
+	parser.add_argument("--anno", 
+		required = True,
+		dest = "anno",
+		type = str,
+		help = "The name of the file containing information on the genes and orthogroups")
+
+	parser.add_argument("--wza", 
+		required = True,
+		dest = "wza",
+		type = str,
+		help = "The name of the file containing the wza scores per gene")
+
+	parser.add_argument("--species", 
+		required = True,
+		dest = "species",
+		type = str,
+		help = "The initials of the annotation species you will use")
+ 		
+	parser.add_argument("--output", 
+		required = True,
+		dest = "output",
+		type = str,
+		help = "Give a name for the output file")
+
+	args = parser.parse_args()
+
+
+	species_dict = {}
+	
+# This dict stores the position index of each species in the annotation file
+	species_index = {}
+## It is assumed that the annotation file has the following structure:
+#Df	Is	Pa	Pi
+	species_list = [ "Pi" , "Is", "Pa", "Pi"]
+## Orthogroup_name     species_1      species_2      species_3      ... species_k
+
+	for focal_species in species_list:
+		count = 0
+
+		focal_species_annotation_dict = {}
+
+		for line in open( args.anno ):
+			count +=1
+
+			if count == 1:
+				for i in range(len(line.strip().split("\t"))):
+					## ignore "Orthogroup" in the header 
+					if i ==0: continue
+					## save the index of each species in the 
+					species_index[ line.strip().split()[i] ] = i 
+				print( species_index )
+				continue
+
+## Need to be specific with split and strip - Orthofinder doesn't give a missing data character
+			line_data = line.strip("\n").split("\t")
+			if len(line_data) != len(species_index.keys())+1:
+				print("missing data in the orthogroup table at line:", count)
+				break
+
+			orthogroup_name = line_data[0]
+
+## make a list of all the genes within the current orthogroup for the focal species
+			species_anno_list = line_data[ species_index[ focal_species ] ].split(", ") 	
+
+## For each gene belonging to the current orthogroup, make an entry in the dict
+			for s_i in species_anno_list:
+				focal_species_annotation_dict[ s_i ] = orthogroup_name
+
+		species_dict[ focal_species ] = focal_species_annotation_dict
+
+	
+	wza_csv = pd.read_csv( args.wza )		
+	
+	OG_dict = {}
+	for index, row in wza_csv.iterrows():
+		try:
+			OG =  species_dict[ args.species ][row['gene']]  # print(row['gene'])	
+		except KeyError:
+## It would be better if we did not have to do the following, if there was a sinlge name for each gene
+			splitter = row['gene'].split(";")[-1]#.split("=")[1])
+			if len( splitter.split("=") ) == 2: 
+				try:
+					OG =  species_dict[ args.species ][splitter.split("=")[1]]  # print(row['gene'])	
+				except KeyError:
+					OG = "NA"
+			else:
+				OG = "NA"
+		## do some string manipulation thing here
+		if OG != "NA":print(OG)
+		
+		OG_dict[row['gene']] = OG
+		
+	
+## Flip the dictionary around to get the number of genes in each orthogroup for my species:
+	flipped = {}
+  
+	for key, value in OG_dict.items():
+	    if value not in flipped:
+	        flipped[value] = [key]
+	    else:
+	        flipped[value].append(key)
+
+	number_OG_dict = {}
+  
+	for key, value in flipped.items():
+		print(key, value)
+		number_OG_dict[key] = len(value)
+# Set it so that genes not assigned to orthogroups have 0 paralogs
+	number_OG_dict["NA"] = 0
+## Make a dict that contains the number of members of each orthogroup
+	
+	## Now map the OG_dict to the WZA file using pandas
+	wza_csv["orthogroup"] = wza_csv["gene"].map(OG_dict)
+	wza_csv["num_genes_in_OG"] = wza_csv["orthogroup"].map(number_OG_dict)
+	
+	wza_csv.to_csv( args.output , index = False, sep = "\t")
+main()
