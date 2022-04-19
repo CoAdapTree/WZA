532bcd602a2a7f03de9d2631a2d204e161cc59f4
Tom Booker <mrtombooker@gmail.com>
2022-04-19 12:57:33 -0700

    adjusted threshold default

--- dataAnalysis/bin/WZA_CoAdapTree_annotatedSNPtables.py
+++ dataAnalysis/bin/WZA_CoAdapTree_annotatedSNPtables.py
@@ -43,7 +43,7 @@ def main():
             required = False,
             dest = "sample_snps",
             type = int,
-            help = "[OPTIONAL] Give the number of SNPs you want to downsample to. Give -1 if you want to use the median number of SNPs. Note that calculating the median within the script is slow, so you may want to run a dummy analysis, get the median number of SNPs then use that explicitly.",
+            help = "[OPTIONAL] Give the number of SNPs you want to downsample to. Give -1 if you want to use the 75th percentile of the number of SNPs. Note that calculating the median within the script is slow, so you may want to run a dummy analysis, get the median number of SNPs then use that explicitly.",
             default = 0)
 
     parser.add_argument("--resamples",
@@ -53,29 +53,46 @@ def main():
             help = "[OPTIONAL] Give the number of times you want to resample WZA scores when the number of SNPs exceeds the sample_snps threshold. [100]",
             default = 100)
 
+    parser.add_argument("--verbose",
+            required = False,
+            action = "store_true",
+            help = "[OPTIONAL] Give this flag if you want to run the script in verbose mode")
+
     args = parser.parse_args()
 
     csv = pd.read_csv(args.correlations, sep = "\t")
 
-    print(csv)
+    if args.verbose:
+        print("here's a peek at the input data")
+        print(csv)
+    csv_genes = csv[csv["attribute"]!="none"]
+    csv_gb_gene = csv_genes.groupby("attribute")
 
     if args.sample_snps == -1:
 ## Get a list of the number of SNPs per contig
-        num_SNP_list = np.array([contigSnpTable(SNPs, annotations[annotations["seqname"] == contig]).shape[0] for contig,SNPs in contigGenerator(args.correlations, env)])
-## Calculate the median number of SNPs per gene
-        max_SNP_count = int(np.median(num_SNP_list[num_SNP_list!=0]))
-        print("Using the median number of SNPs as the maximum in each gene:", max_SNP_count)
+
+        csv_gb_gene_SNP_count = csv_genes.groupby("attribute")
+
+        num_SNP_list = np.array( [s[1].shape[0] for s in csv_gb_gene_SNP_count] )
+
+#        num_SNP_list = np.array([contigSnpTable(SNPs, annotations[annotations["seqname"] == contig]).shape[0] for contig,SNPs in contigGenerator(args.correlations, env)])
+
+## Calculate the 75th percentile of SNPs per gene
+        max_SNP_count = int(np.percentile(num_SNP_list[num_SNP_list!=0], 75))
+
+        if args.verbose:
+            print("Using the 75th percentile number of SNPs as the maximum in each gene:", max_SNP_count)
 
     elif args.sample_snps == 0:
         max_SNP_count = int(1e6) # This is just a large number that is never going to be the number of SNPs within a gene
-        print("The maximum number of SNPs in each gene:", max_SNP_count)
+        if args.verbose:
+            print("The maximum number of SNPs in each gene:", max_SNP_count)
 
     else:
         max_SNP_count = int(args.sample_snps)
-        print("The maximum number of SNPs in each gene:", max_SNP_count)
+        if args.verbose:
+            print("The maximum number of SNPs in each gene:", max_SNP_count)
 
-    csv_genes = csv[csv["attribute"]!="none"]
-    csv_gb_gene = csv_genes.groupby("attribute")
 
     all_genes = []
 
@@ -97,7 +114,8 @@ def main():
 
 #        print(wza_emp_pVal)
 
-        print( count )
+        if args.verbose:
+            print("gene:", count )
 
         output = {"contig": list(gene_df.contig)[0],
                     "env":list(gene_df.env)[0],
